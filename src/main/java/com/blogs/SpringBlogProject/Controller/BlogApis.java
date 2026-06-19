fd9ffc4484da3e0a3a2de977eb0acb64a97b3a92
codingwithriya <itisghoshriya@gmail.com>
2026-06-19 23:46:59 +0530

    c2

--- src/main/java/com/blogs/SpringBlogProject/Controller/BlogApis.java
+++ src/main/java/com/blogs/SpringBlogProject/Controller/BlogApis.java
@@ -40,10 +40,11 @@ public class BlogApis {
 
     @PostMapping("/save")
 
-    
+
     public String saveBlog(@ModelAttribute("blog") Blog blog){
         b1.save(blog);
         return "redirect:/";
+        
     }
 
     @GetMapping("/edit/{id}")
